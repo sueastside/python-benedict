0c3ab19c7348ba47981e4cc1f5a07f530fe68a82
sueastside <sueastside@gmail.com>
2022-09-28 17:29:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ setup(
         #"python-dateutil >= 2.8.0, < 3.0.0",
         #"python-fsutil >= 0.6.0, < 1.0.0",
         #"python-slugify >= 6.0.1, < 7.0.0",
-        #"pyyaml >= 6.0, < 7.0",
+        "pyyaml >= 6.0, < 7.0",
         #"requests >= 2.26.0, < 3.0.0",
         #"toml >= 0.10.2, < 1.0.0",
         #"xmltodict >= 0.12.0, < 1.0.0",
